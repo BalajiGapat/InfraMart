56e002c0092f7f0b754966f90d86a68a27452bd6
Balaji Gapat <gapatbalaji2901@gmail.com>
2023-09-22 21:17:06 +0530

    InfraMart V1.1

---
+++ Frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,17 @@
+
+
+const AboutUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+
+    <pre>
+  <h1>Welcome to Inframart</h1>
+     your one-stop destination for all your infrastructure material needs. At Inframart, we are committed to revolutionizing the way you source, purchase, and manage infrastructure materials.
+    </pre>
+
+     
+    </div>
+  );
+};
+
+export default AboutUs;
